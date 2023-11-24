3c87c2d26df2eefd67d9d521eb5a9a65f099bb3d
Lara <larapc2004@gmail.com>
2023-11-24 22:30:57 +0000

    Added interactable test
    Co-authored-by: Francisco <up202208485@up.pt>

---
+++ src/test/java/model/InteractableTest.java
@@ -0,0 +1,23 @@
+package model;
+import model.game.elements.Interactable;
+import model.game.elements.Item;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class InteractableTest {
+
+        @Test
+        public void contains(){
+            Interactable interactable = new Item(1,2,3,4,"new item");
+            assertTrue(interactable.contains(new Position(3,5)));
+            assertTrue(interactable.contains(new Position(4,3)));
+            assertTrue(interactable.contains(new Position(3,6)));
+            assertTrue(interactable.contains(new Position(4,6)));
+            assertFalse(interactable.contains(new Position(5,3)));
+            assertFalse(interactable.contains(new Position(3,7)));
+            assertFalse(interactable.contains(new Position(5,7)));
+    }
+}
