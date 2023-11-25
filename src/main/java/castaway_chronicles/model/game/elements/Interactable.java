0a79e45d3467920f9d03645e6614d438ee73bdb6
Bruno <up202205619@up.pt>
2023-11-25 14:10:53 +0000

    Fixed Interactable and improved InteractableTest

--- src/main/java/castaway_chronicles/model/game/elements/Interactable.java
+++ src/main/java/castaway_chronicles/model/game/elements/Interactable.java
@@ -5,8 +5,8 @@ import castaway_chronicles.model.Position;
 public abstract class Interactable extends Element {
     public Interactable(int x, int y, int w, int h, String name) {super (x, y, w, h, name);}
     public boolean contains(Position position) {
-        return (position.getX() >= this.getPosition().getX() && position.getX() <= this.getPosition().getX() + this.getWidth()
-            && position.getY() >= this.getPosition().getY() && position.getY() <= this.getPosition().getY() + this.getHeight());
+        return (position.getX() >= this.getPosition().getX() && position.getX() < this.getPosition().getX() + this.getWidth()
+            && position.getY() >= this.getPosition().getY() && position.getY() < this.getPosition().getY() + this.getHeight());
     }
     @Override
     public boolean equals(Object o) {
--- src/test/java/castaway_chronicles/model/InteractableTest.java
+++ src/test/java/castaway_chronicles/model/InteractableTest.java
@@ -12,12 +12,18 @@ public class InteractableTest {
         @Test
         public void contains(){
             Interactable interactable = new Item(1,2,3,4,"new item");
+            assertTrue(interactable.contains(new Position(2,4)));
+            assertTrue(interactable.contains(new Position(3,2)));
+            assertTrue(interactable.contains(new Position(2,5)));
             assertTrue(interactable.contains(new Position(3,5)));
-            assertTrue(interactable.contains(new Position(4,3)));
-            assertTrue(interactable.contains(new Position(3,6)));
-            assertTrue(interactable.contains(new Position(4,6)));
-            assertFalse(interactable.contains(new Position(5,3)));
-            assertFalse(interactable.contains(new Position(3,7)));
-            assertFalse(interactable.contains(new Position(5,7)));
+            assertFalse(interactable.contains(new Position(4,2)));
+            assertFalse(interactable.contains(new Position(2,6)));
+            assertFalse(interactable.contains(new Position(4,6)));
+            Interactable interactable2 = new Item(1,2,1,1,"new item");
+            assertTrue(interactable2.contains(new Position(1,2)));
+            assertFalse(interactable2.contains(new Position(1,1)));
+            assertFalse(interactable2.contains(new Position(1,3)));
+            assertFalse(interactable2.contains(new Position(0,2)));
+            assertFalse(interactable2.contains(new Position(2,2)));
     }
 }
