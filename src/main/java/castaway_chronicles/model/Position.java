4c666cb591147ecc86d5259b07f037a05b9d964f
Lara <larapc2004@gmail.com>
2023-11-25 15:22:41 +0000

    Added equals and hash code methods to Position and its tests

--- src/main/java/castaway_chronicles/model/Position.java
+++ src/main/java/castaway_chronicles/model/Position.java
@@ -1,5 +1,7 @@
 package castaway_chronicles.model;
 
+import java.util.Objects;
+
 public class Position {
     private final int x;
     private final int y;
@@ -13,4 +15,15 @@ public class Position {
     public Position getDown(int offset) {return new Position(x, y + offset);}
     public int getX() {return x;}
     public int getY() {return y;}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Position)) return false;
+        Position position = (Position) o;
+        return position.x == x && position.y == y;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
--- src/test/java/castaway_chronicles/model/PositionTest.java
+++ src/test/java/castaway_chronicles/model/PositionTest.java
@@ -2,8 +2,10 @@ package castaway_chronicles.model;
 
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PositionTest {
 
@@ -28,5 +30,38 @@ public class PositionTest {
     void getDown(@ForAll int x, @ForAll int y, @ForAll int o) {
         assertEquals(x, new Position(x, y).getDown(o).getX());
         assertEquals(y + o, new Position(x, y).getDown(o).getY());
+    }
+
+    @Test
+    public void equals_same(){
+        Position pos1 = new Position(1, 2);
+        Position pos2 = new Position(1,2);
+        assertEquals(pos1, pos2);
+        assertEquals(pos1, pos1);
+    }
+
+    @Test
+    public void equals_different(){
+        Position pos1 = new Position(1, 2);
+        Position pos2 = new Position(1,3);
+        assertNotEquals(pos1, pos2);
+        assertNotEquals(pos1, new Object());
+    }
+
+
+    @Test
+    public void hashcode_same(){
+        Position pos1 = new Position(1, 2);
+        Position pos2 = new Position(1,2);
+        assertEquals(pos1.hashCode(), pos2.hashCode());
+        assertEquals(pos1.hashCode(), pos1.hashCode());
+    }
+
+    @Test
+    public void hashcode_different(){
+        Position pos1 = new Position(1, 2);
+        Position pos2 = new Position(1,3);
+        assertNotEquals(pos1.hashCode(), pos2.hashCode());
+        assertNotEquals(pos1.hashCode(), new Object().hashCode());
     }
 }
