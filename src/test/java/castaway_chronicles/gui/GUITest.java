db936a635dda2231b739cbe35997edc5a05f867b
Lara <larapc2004@gmail.com>
2023-11-26 23:17:18 +0000

    Updated tests
    Co-authored-by: Bruno <up202205619@up.pt>
    Co-authored-by: Francisco <up202208485@up.pt>

--- src/test/java/castaway_chronicles/gui/GUITest.java
+++ src/test/java/castaway_chronicles/gui/GUITest.java
@@ -4,7 +4,6 @@ import castaway_chronicles.model.Position;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.BasicTextImage;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/castaway_chronicles/view/ImagesTest.java
+++ src/test/java/castaway_chronicles/view/ImagesTest.java
@@ -1,6 +1,5 @@
 package castaway_chronicles.view;
 
-import com.googlecode.lanterna.graphics.TextImage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
