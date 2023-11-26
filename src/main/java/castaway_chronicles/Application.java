ed3eae71ff7e41fd8fa21283654039417b3b4877
Lara <larapc2004@gmail.com>
2023-11-26 23:18:13 +0000

    Added testing code to application
    Co-authored-by: Bruno <up202205619@up.pt>
    Co-authored-by: Francisco <up202208485@up.pt>

--- src/main/java/castaway_chronicles/Application.java
+++ src/main/java/castaway_chronicles/Application.java
@@ -1,4 +1,48 @@
 package castaway_chronicles;
 
+import castaway_chronicles.gui.LanternaGUI;
+import castaway_chronicles.model.Position;
+import castaway_chronicles.model.game.scene.Location;
+import castaway_chronicles.model.game.scene.SceneLoader;
+import castaway_chronicles.view.Images;
+import castaway_chronicles.view.game.LocationViewer;
+import castaway_chronicles.view.game.SceneViewer;
+
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class Application {
+    private final LanternaGUI gui;
+
+    public Application() throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new LanternaGUI(200, 150);
+    }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException, InterruptedException {
+        new Application().start();
+    }
+
+    private void start() throws IOException, URISyntaxException, InterruptedException {
+        SceneLoader loadscene = new SceneLoader("Beach", "Location");
+        List<String> interactables = new ArrayList<>();
+        interactables.add("toot");
+        List<String> backgrounds = new ArrayList<>();
+        backgrounds.add("Beach");
+        Images interactablesImages = new Images(interactables, "Interactables");
+        Images backgroundImages = new Images(backgrounds, "Backgrounds");
+        HashMap<String, Images> images = new HashMap<>();
+        images.put("Interactables", interactablesImages);
+        images.put("Backgrounds", backgroundImages);
+        SceneViewer<Location> scene = new LocationViewer((Location) loadscene.createScene(), images);
+        scene.draw(gui);
+        gui.drawText(new Position(10, 100), 100, "Hello");
+        TimeUnit.SECONDS.sleep(15);
+        gui.close();
+    }
 }
