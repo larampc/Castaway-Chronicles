4912009a0587c45dac132e73ac9dcca6da5ed71f
Francisco <124478712+Francis-coder01@users.noreply.github.com>
2023-11-26 16:34:50 +0000

    Revert "Scene loader"

--- src/main/java/castaway_chronicles/model/game/elements/NPC.java
+++
@@ -1,7 +0,0 @@
-package castaway_chronicles.model.game.elements;
-
-public class NPC extends Interactable {
-    public NPC(int x, int y, int w, int h, String name) {
-        super(x, y, w, h, name);
-    }
-}
