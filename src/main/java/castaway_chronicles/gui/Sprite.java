6fcb97aa35a70d7862fffd6876a3c6d6748a66f6
Lara <larapc2004@gmail.com>
2023-11-29 14:46:30 +0000

    Added class Sprite

---
+++ src/main/java/castaway_chronicles/gui/Sprite.java
@@ -0,0 +1,34 @@
+package castaway_chronicles.gui;
+
+import castaway_chronicles.model.Position;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Sprite {
+    private final BufferedImage sprite;
+    public Sprite(File fontFile) throws IOException {
+        sprite = ImageIO.read(fontFile);
+    }
+    public void drawSprite(Position position, TextGraphics graphics) {
+        for (int x = 0; x < sprite.getWidth(); x++){
+            for (int y = 0; y < sprite.getHeight(); y++){
+                int a = sprite.getRGB(x, y);
+                int alpha = (a >> 24) & 0xff;
+                int red = (a >> 16) & 255;
+                int green = (a >> 8) & 255;
+                int blue = a & 255;
+                if (alpha != 0) {
+                    TextCharacter c = new TextCharacter(' ', new TextColor.RGB(red, green, blue), new TextColor.RGB(red, green, blue));
+                    graphics.setCharacter(position.getX() + x, position.getY() + y, c);
+                }
+            }
+        }
+    }
+    public int getWidth() {return sprite.getWidth();}
+    public int getHeight() {return sprite.getHeight();}
+}
