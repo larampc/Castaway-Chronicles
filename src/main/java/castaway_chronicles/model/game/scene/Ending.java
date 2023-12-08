61b72e3ac23ad2eb2dae46a58d3fdfd4f608273b
Bruno <up202205619@up.pt>
2023-12-08 19:08:30 +0000

    Added the drink ending

--- src/main/java/castaway_chronicles/model/game/scene/Ending.java
+++ src/main/java/castaway_chronicles/model/game/scene/Ending.java
@@ -2,7 +2,7 @@ package castaway_chronicles.model.game.scene;
 
 public class Ending {
     private String name;
-    private int current = 0;
+    private int current = 1;
     public Ending(String name) {this.name = name;}
 
     public int getCurrent() {
@@ -13,7 +13,8 @@ public class Ending {
         this.current = current;
     }
     public String getCurrentFrame() {
-        return name + current;
+        return String.format("%04d", current);
+//        return name + current;
     }
     public String getName() {return name;}
 }
