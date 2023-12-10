4b27c85f072a629b71f0daf9e5604c6d4b5cd9e3
Lara <larapc2004@gmail.com>
2023-12-10 22:29:55 +0000

    Save endings fixed

--- src/main/java/castaway_chronicles/model/menu/EndingPage.java
+++ src/main/java/castaway_chronicles/model/menu/EndingPage.java
@@ -3,10 +3,12 @@ package castaway_chronicles.model.menu;
 import castaway_chronicles.model.game.elements.Element;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,9 +25,8 @@ public class EndingPage {
             if (s.length == 6) visibleEndings.add(new EndingItem(Integer.parseInt(s[1]), Integer.parseInt(s[2]), Integer.parseInt(s[3]), Integer.parseInt(s[4]), s[0]));
             else endings.add(new EndingItem(Integer.parseInt(s[1]), Integer.parseInt(s[2]), Integer.parseInt(s[3]), Integer.parseInt(s[4]), s[0]));
         }
-        resource = getClass().getClassLoader().getResource("achieved_endings.txt");
-        assert resource != null;
-        br = new BufferedReader(new FileReader(resource.getFile(), StandardCharsets.UTF_8));
+        File endings = new File(Paths.get("").toAbsolutePath()+"/src/main/resources/achieved_endings.txt");
+        br = new BufferedReader(new FileReader(endings, StandardCharsets.UTF_8));
         for (String line; (line = br.readLine()) != null; ) {
             int i = index(line);
             visibleEndings.remove(i);
