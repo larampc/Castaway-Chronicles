fb31b395f6658f72d49ee7081d3ca9eb40e2233c
Lara <larapc2004@gmail.com>
2023-12-10 09:16:36 +0000

    Corrected bug in loop

--- src/main/java/castaway_chronicles/model/game/scene/SceneLoader.java
+++ src/main/java/castaway_chronicles/model/game/scene/SceneLoader.java
@@ -40,7 +40,7 @@ public class SceneLoader {
                 String[] s = line.split(" ", -1);
                 if(s.length < 6) return null;
                 int x = Integer.parseInt(s[2]), y = Integer.parseInt(s[3]), w = Integer.parseInt(s[4]), h = Integer.parseInt(s[5]);
-                boolean loopable = s.length == 6;
+                boolean loopable = s.length == 7;
                 return new Background(x, y, w, h, s[1], loopable);
             }
         }
