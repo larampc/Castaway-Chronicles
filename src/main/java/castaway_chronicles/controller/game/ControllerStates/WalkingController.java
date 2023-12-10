93ff977c5e6dcdd1ff7211fbe0729ede9d39bae4
Lara <larapc2004@gmail.com>
2023-12-10 09:20:19 +0000

    Corrected bug in loop

--- src/main/java/castaway_chronicles/controller/game/ControllerStates/WalkingController.java
+++ src/main/java/castaway_chronicles/controller/game/ControllerStates/WalkingController.java
@@ -30,11 +30,11 @@ public class WalkingController implements ControllerState{
         int offset = (character_x - walkto.getX()) + location.getMainChar().getWidth()/2;
         int next_x = background_x + location.getMainChar().getWidth()/2 + offset;
         goRight = (offset < 0);
-        if (location.getBackground().isIsloopable()) {toWalk = offset/dx; return true;}
         if (abs(offset) < 2*dx || (background_x == 0 && !goRight) || (background_x-dx <= max_background_x && goRight)) {
             toWalk = 0;
             return false;
         }
+        if (location.getBackground().isIsloopable()) {toWalk = offset/dx; return true;}
         if (next_x <= 0 && next_x >= max_background_x) {
             toWalk = offset/dx;
             toWalk += (toWalk < 0) ? 1 : -1;
