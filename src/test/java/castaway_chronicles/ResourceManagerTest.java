7164f6d33d4ee40664a2a1c931924cc532d21cc6
Francisco <aa@aa>
2023-12-20 22:54:45 +0000

    Working on ResourceManagerTest

--- src/test/java/castaway_chronicles/ResourceManagerTest.java
+++ src/test/java/castaway_chronicles/ResourceManagerTest.java
@@ -3,36 +3,84 @@ package castaway_chronicles;
 import org.junit.jupiter.api.*;
 
 import java.io.*;
-
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ResourceManagerTest {
 
     private File newFile;
-    @BeforeAll
-    void init() throws IOException {
-//        newFile = new File(Paths.get("src/main/resources/resourceTest").toString());
+    private File newDir;
+    private ResourceManager resourceManager;
+    @BeforeEach
+    void init() {
+        resourceManager = ResourceManager.getInstance();
+        newFile = new File("src/main/resources/Testing/resourceTest.txt");
+        newDir = new File("src/main/resources/Testing/resourceDir");
+        try {
+            boolean success = newFile.createNewFile();
+            assertTrue(success);
+            success = newDir.mkdir();
+            assertTrue(success);
+            success = new File(newDir.getPath() + "/file1.txt").createNewFile();
+            assertTrue(success);
+            success = new File(newDir.getPath() + "/file2.txt").createNewFile();
+            assertTrue(success);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        resourceManager.setPath("Testing/resourceTest.txt");
+        resourceManager.writeToFile("lorem ipsem\ntesting\nresource\nmanager\n");
     }
 
-    @AfterAll
-    void tearDown() throws IOException {
-//        newFile.delete();
+    @AfterEach
+    void tearDown() {
+        boolean success =  newFile.delete();
+        assertTrue(success);
+        resourceManager.setPath("resourceDir");
+        resourceManager.deleteResourceFileContent();
+        success = newDir.delete();
+        assertTrue(success);
     }
 
     @Test
+    void existsStaticResourceFile(){
+        resourceManager.setPath("Testing/staticResource.txt");
+        assertTrue(resourceManager.existsStaticResourceFile());
+
+        resourceManager.setPath("Testing/staticResource2.txt");
+        assertFalse(resourceManager.existsStaticResourceFile());
+    }
+
+    @Test
+    void readStaticResourceFile() {
+        resourceManager.setPath("Testing/staticResource.txt");
+        assertEquals(List.of("lorem ipsem","testing","resource","manager"), resourceManager.readCurrentTimeResourceFile());
+    }
+
+    @Test
+    void failToReadStaticResourceFile() {
+        resourceManager.setPath("Testing/resourceTest2.txt");
+
+        Assertions.assertThrows(RuntimeException.class, resourceManager::readCurrentTimeResourceFile);
+    }
+    @Test
     void readCurrentTimeResourceFile() {
-        ResourceManager resourceManager = ResourceManager.getInstance();
-        resourceManager.setPath("Testing/resourceTest");
-        List<String> lines = resourceManager.readCurrentTimeResourceFile();
-        assertEquals(List.of("lorem ipsem","testing","resource","manager"),lines);
+        assertEquals(List.of("lorem ipsem","testing","resource","manager"),resourceManager.readCurrentTimeResourceFile());
+
+        resourceManager.writeToFile("changed");
+
+        assertEquals(List.of("lorem ipsem","testing","resource","manager","changed"),resourceManager.readCurrentTimeResourceFile());
     }
 
     @Test
     void failToReadCurrentTimeResourceFile() {
-        ResourceManager resourceManager = ResourceManager.getInstance();
+        resourceManager.setPath("Testing/resourceTest2");
+
+        Assertions.assertThrows(RuntimeException.class, resourceManager::readCurrentTimeResourceFile);
+    }
+    @Test
+    void countFiles() {
         resourceManager.setPath("Testing/resourceTest2");
 
         Assertions.assertThrows(RuntimeException.class, resourceManager::readCurrentTimeResourceFile);
